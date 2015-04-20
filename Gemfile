5930d622e4c434772af25b84bdb26f48909af650
Amaury MASSELIN <amaury.masselin@laposte.net>
2015-04-20 02:16:08 +0200

    Layouts

--- Gemfile
+++ Gemfile
@@ -40,11 +40,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin]
 
 gem 'heroku'
 
-group :production, :staging do
-  #gem 'pg'
-end
-
-group :development, :test do
   gem 'sqlite3'
   gem 'sqlite3-ruby', '~> 1.3.0', :require => 'sqlite3'
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  match '/contact', :to => 'pages#contact', via: :get
+  match '/about',   :to => 'pages#about', via: :get
+  match '/help',    :to => 'pages#help', via: :get
   get 'pages/home'
-
-  get 'pages/contact'
-
+  root :to => 'pages#home', via: :get
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
